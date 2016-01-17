557667a9adf6f27c33a7786916ebfc82783b845a
mamunul <mamunul@192.168.1.135>
2016-01-17 18:07:59 +0600

    adaboost wrong implementation updated

--- violajones.m
+++ violajones.m
@@ -20,89 +20,78 @@ y1 = ones(1,m);
 y2 = zeros(1,l);
 y = [y1,y2];
 
-
-feature_list= haarfeature(24);
+feature_count = 20000;
+feature_list= haarfeature(24,feature_count);
 
 [feature_count n] = size(feature_list);
 
-% feature_count = 50000;
-result = zeros(feature_count,13);
+
+
 
 i = 0;
 
+feature_values=0;
 for f_no =1:feature_count
     
     feature = feature_list(f_no,:);
- 
-    lowest_err = 100;
-    lowest_t = 0;
     
-    
-    w(1,1:m+l) = initialize_weight(m,l);
-    for p = 1:2
-        
-    polarity = -1;
-    if p == 2
-        polarity = 1;
-    end
-    theta(1) = 1;
-    max = 100 - theta(1);
+    f_I = calculate_feature_value(I,feature,y);
+    feature_values(1:(m+l),1:2,f_no) = f_I;
+end
+
+
+NumberOfClassifier = 50;
 %     max = 3;
-    for t = 1:max
-        
-        if t >1
-            theta(t) = theta(t-1)+1;
-        end
+result = zeros(NumberOfClassifier,13);
+w(1,1:m+l) = initialize_weight(m,l);
+for t = 1:NumberOfClassifier
     
-        h(t,:) = CheckForFeature(feature,I,theta(t),polarity);
+    w(t,1:m+l) = normalize_weight(w(t,:));
+    
+    [Tp Tm] = calculate_tpm1(w(t,:),y);
+    
+   
+    
+    err = 100;
+    thresh = 0;
+    polar = 0;
+    for f_no =1:feature_count
+        
        
-        epsilon(t) =calc_eps(h(t,:),y,w(t,:));
-        alpha(t) = calc_alpha(epsilon(t));
         
-        error(t) = calculate_error(h(t,:),y,w(t,:));
+        feature = feature_list(f_no,:);
         
-        w(t+1,:) = calc_weight(w(t,:),alpha(t),h(t,:),y);
+        values = feature_values(:,:,f_no);
         
-        if(error(t) < lowest_err)
-            lowest_err = error(t);
-            lowest_t = t;
-        end
+        fv_weight = fvplusweight(values,w(t,:));
         
-        %             if error(t)>error(t-1)
-        %
-        %                 break;
-        %             end
-        
+
+        [err thresh polar] = bestfeature(feature,fv_weight,Tp,Tm,err,thresh,polar);
+
     end
+  
+
+    beta(t) = calc_beta(err);
+    alpha(t) = calc_alpha(beta(t));
+    error(t) = err;
+    theta(t)=thresh;
+    polarity(t)=polar;
+    w(t+1,:) = calc_weight(w(t,:),beta(t),values,thresh,polar);
     
-    
-    display('feature no------------');
-    disp(f_no);
-    %     display('feature------------');
-    %     disp(feature);
-    %     display('w------------');
-    %     disp(w(lowest_t,:));
-    %     display('theta------------');
-    %     disp(theta(lowest_t));
-    %     display('h------------');
-    %     disp(h(lowest_t,:));
-    %     display('alpha------------');
-    %     disp(alpha(lowest_t));
-    %     display('epsilon------------');
-    %     disp(epsilon(lowest_t));
-    %     display('error------------');
-    %     disp(error(lowest_t));
-    value_no = f_no;
+    display('classifier no------------');
+    disp(t);
+
+    value_no = t;
     value_x = double(feature(1));
     value_y = double(feature(2));
     value_fw = double(feature(5));
     value_fh = double(feature(6));
     value_w = double(feature(3));
     value_h = double(feature(4));
- 
-    i = i+1;
-    result(i,:)=[f_no,lowest_t,error(lowest_t),theta(lowest_t),alpha(lowest_t),epsilon(lowest_t),polarity,value_x ,value_y,value_fw ,value_fh,value_w ,value_h];
-    end 
+    
+   
+    result(t,:)=[t,t,err,thresh,alpha(t),beta(t) ,polar,value_x ,value_y,value_fw ,value_fh,value_w ,value_h];
+    %     end
     
 end
 
@@ -115,6 +104,118 @@ R = B(1:10000,:);
 
 endtime = generate_datetime();
 generateTrainingDb(R,starttime,endtime,trainig_face_limit,feature_count);
+
+end
+
+function fvw = fvplusweight(v,w)
+
+[m n] = size(v);
+
+for i = 1:m
+    
+    fvw(i,1)= v(i,1);
+    fvw(i,2)= v(i,2);
+    fvw(i,3)= w(i);
+end
+
+
+end
+
+function beta = calc_beta(error)
+
+beta = error/(1-error);
+
+end
+
+function [error thresh polarity] = bestfeature(feature,feature_value,tp,tm,error,thresh,polarity)
+sp = 0;
+sm = 0;
+[m n]=size(feature_value);
+feature_value = sortrows(feature_value,1);
+
+for i = 1:m
+    
+
+    
+    if feature_value(i,2) == 1
+        sp = sp + feature_value(i,3);
+    else
+        sm = sm + feature_value(i,3);
+    end
+    
+    v1 = sp+tm-sm;
+    v2 = sm+tp-sp;
+    
+    e = min(v1,v2);
+    
+       
+    if e<error
+        error = e;
+        thresh = feature_value(i,1);
+        
+        if e == v1
+            polarity = -1;
+        else
+            polarity = 1;
+        end
+    end
+end
+
+
+end
+
+function [tp tm]=calculate_tpm1(w,y)
+
+tp = 0;
+tm = 0;
+[m n] = size(w);
+
+for i = 1:n
+if y(i) == 1
+    tp = tp+ w(i);
+ 
+else
+    tm = tm + w(i);
+  
+end
+end
+
+end
+
+function [tp tm]=calculate_tpm(w,y,values)
+
+tp = 0;
+tm = 0;
+[m n] = size(w);
+w_p = 0;w_n = 0;
+for i = 1:n
+if y(i) == 1
+    tp = tp+ w(i)*values(i);
+    w_p = w_p + w(i);
+else
+    tm = tm + w(i)*values(i);
+    w_n = w_n + w(i);
+end
+end
+
+tp = tp/w_p;
+tm = tp/w_m;
+end
+
+function w = normalize_weight(wp)
+
+[m n] = size(wp);
+sum = 0;
+for i = 1: n
+    
+    sum = sum + wp(i);
+end
+
+
+for i = 1: n
+    
+    w(i) = wp(i)/sum;
+end
 
 end
 
@@ -151,17 +252,17 @@ docNode.getDocumentElement.appendChild(sin13);
 
 sin1 = create_single_node(docNode,entry_node,'count',m);
 docNode.getDocumentElement.appendChild(sin1);
-% 
+%
 sin2 = create_single_node(docNode,entry_node,'start-time',starttime);
 docNode.getDocumentElement.appendChild(sin2);
-% 
+%
 sin3 = create_single_node(docNode,entry_node,'end-time',endtime);
 docNode.getDocumentElement.appendChild(sin3);
 
 for i = 1:m
-
-  feature_node =  create_feature_node(docNode,entry_node,R(i,:));
-  entry_node.appendChild(feature_node);
+    
+    feature_node =  create_feature_node(docNode,entry_node,R(i,:));
+    entry_node.appendChild(feature_node);
 end
 
 xmlFileName = ['facedetection','.xml'];
@@ -172,27 +273,27 @@ end
 
 function feature_node = create_feature_node(docNode,entry_node,f)
 
- rf_no = f(1);
- lowest_t = f(2);
- error = f(3);
- theta = f(4);
- alpha = f(5);
- epsilon = f(6);
- polarity = f(7);
- value_x = f(8);
- value_y = f(9);
- value_fw = f(10);
- value_fh = f(11);
- value_w = f(12);
- value_h = f(13);
- 
- feature_node = docNode.createElement('feature');
- entry_node.appendChild(feature_node);
- 
- sin1 = create_single_node(docNode,feature_node,'error',error);
- sin2 = create_single_node(docNode,feature_node,'theta',theta);
- 
- sin4 = create_single_node(docNode,feature_node,'polarity',polarity);
+rf_no = f(1);
+lowest_t = f(2);
+error = f(3);
+theta = f(4);
+alpha = f(5);
+epsilon = f(6);
+polarity = f(7);
+value_x = f(8);
+value_y = f(9);
+value_fw = f(10);
+value_fh = f(11);
+value_w = f(12);
+value_h = f(13);
+
+feature_node = docNode.createElement('feature');
+entry_node.appendChild(feature_node);
+
+sin1 = create_single_node(docNode,feature_node,'error',error);
+sin2 = create_single_node(docNode,feature_node,'theta',theta);
+
+sin4 = create_single_node(docNode,feature_node,'polarity',polarity);
 sin5 = create_single_node(docNode,feature_node,'x',value_x);
 sin6 = create_single_node(docNode,feature_node,'y',value_y);
 sin7 = create_single_node(docNode,feature_node,'fw',value_fw);
@@ -200,17 +301,17 @@ sin8 = create_single_node(docNode,feature_node,'fh',value_fh);
 sin9 = create_single_node(docNode,feature_node,'w',value_w);
 sin10 = create_single_node(docNode,feature_node,'h',value_h);
 
- feature_node.appendChild(sin1);
- feature_node.appendChild(sin2);
+feature_node.appendChild(sin1);
+feature_node.appendChild(sin2);
 
- feature_node.appendChild(sin4);
- feature_node.appendChild(sin5);
- feature_node.appendChild(sin6);
- feature_node.appendChild(sin7);
- feature_node.appendChild(sin8);
- feature_node.appendChild(sin9);
- feature_node.appendChild(sin10);
-    
+feature_node.appendChild(sin4);
+feature_node.appendChild(sin5);
+feature_node.appendChild(sin6);
+feature_node.appendChild(sin7);
+feature_node.appendChild(sin8);
+feature_node.appendChild(sin9);
+feature_node.appendChild(sin10);
+
 end
 
 function ct_node = create_single_node(docNode,entry_node,node,text)
@@ -233,6 +334,10 @@ for i = 1:size(TrainFiles,1)
         m=m+1;
         I(:,:,m) = intimg(24,image);
         
+        if m == 10
+        skjfkj=0;
+        end
+        
         if m == limit
             break;
         end
@@ -241,34 +346,34 @@ for i = 1:size(TrainFiles,1)
 end
 end
 
-function nw = calc_weight(w,alpha,h,y)
+function nw = calc_weight(w,beta,values,thresh,p)
 
 [a b] = size(w);
-Z = 0;
 for i = 1:b
-    
-    if h(i) == y(i)
-        q(i) = exp(-alpha);
+   
+    if (p * values(i,1)) < (p * thresh)
+        c = 1;
     else
-        q(i) = exp(alpha);
+        c = 0;
     end
-    
-    Z = Z + w(i)*q(i);
+    if values(i,2) == c
+  
+        nw(i) = w(i) * beta;
+        
+    else
+        nw(i) = w(i);
+    end
 end
 
 
-for i = 1:b
-    
-    nw(i) = (w(i) * q(i))/ Z;
-end
 
 
 end
 
-function alpha = calc_alpha(eps)
+function alpha = calc_alpha(beta)
 
 
-alpha = (1/2) * (log((1-eps)/eps));
+alpha =  log(1/beta);
 
 end
 
@@ -318,7 +423,7 @@ for i = 1:n
         Tm = Tm + w(i);
     end
     
-    if h(i) == 1
+    if h(i) == 1%should be 0
         
         if y(i) == 1
             Sp = Sp + w(i);
@@ -332,7 +437,7 @@ error = min(Sp+Tm-Sm,Sm+Tp-Sp);
 
 end
 
-function feature_list = haarfeature(window)
+function feature_list = haarfeature(window,limit)
 
 
 features = [2,1;1,2;3,1;1,3;2,2];
@@ -359,40 +464,49 @@ for f=1:featureQuantity
                     feature_list(t,4) = height;
                     feature_list(t,5) = fw;
                     feature_list(t,6) = fh;
+                    
+                    if t ==limit
+                        break;
+                    end
+                end
+                if t ==limit
+                    break;
                 end
             end
+            if t ==limit
+                break;
+            end
+        end
+        if t ==limit
+            break;
         end
     end
+    if t ==limit
+        break;
+    end
+    
 end
 end
 
-function  h = CheckForFeature(f,Integral,theta,polarity)
-
-
+function f_value = calculate_feature_value(Integral,f,label)
 fw = f(5);
 fh = f(6);
 width = f(3);
 height = f(4);
 x = f(1);
 y = f(2);
-
+f_value = 0;
 Area = zeros(fw*fh+1,1);
 %                     Area = zeros(4,1);
 dx = width/fw;
 dy = height/fh;
 
-
-% if theta == 55
-%    
-%     theta;
-% end
-
-% threshhold = 0.5;
 [asf kjh number_of_image]=size(Integral);
 
 for i=1:number_of_image
     I = Integral(:,:,i);
     p = 0;
+    
     for nx=x:dx:(x+width-dx)
         
         for ny=y:dy:(y+height-dy)
@@ -415,59 +529,38 @@ for i=1:number_of_image
         end
     end
     
-    h(i) = 0;
     
     if p == 2
         
         AN1 = Area(1);
         AN2 = Area(2);
-        if AN2/AN1 <=1/(1+(theta*0.01)) && polarity == 1
-            
-            h(i) = 1;
-            
-        end
-        
-        if AN2/AN1 >=(1+(theta*0.01)) && polarity == -1
-            
-            h(i) = 1;
-            
-        end
+        Ar = AN1-AN2;
         
     elseif p == 3
         
         AN = Area(3)+Area(1);
         AN1 = AN/2;
         
-       
+        
         AN2 = Area(2);
         
-        if AN2/AN1 <=1/(1+(theta*0.01)) && polarity == 1
-            h(i) = 1;
-            
-        end
-        if AN2/AN1 >=(1+(theta*0.01)) && polarity == -1
-            h(i) = 1;
-            
-        end
-    elseif p == 4 
+        Ar = AN-AN2;
+        
+    elseif p == 4
         
         AN1 = Area(1) + Area(4);
         AN2 = Area(2) + Area(3);
         
-        if AN2/AN1 <=1/(1+(theta*0.01)) &&  polarity == 1
-            h(i) = 1;
-           
-        end
-        if AN2/AN1 >=(1+(theta*0.01)) && polarity == -1
-            h(i) = 1;
-            
-        end
-        
+        Ar = AN1-AN2;
     end
     
+    if abs(Ar) > 3
+        jk=0;
+    end
+    f_value(i,1) = Ar;
+    f_value(i,2) = label(i);
 end
 
-Area(p+1)=h(i)*1000000;
 
 end
 
@@ -485,6 +578,8 @@ if(k == 3)
     i = rgb2gray(i);
     [m,n,k] = size(i);
 end
+
+i = normalize_image(i);
 
 I(1:m,1:n) = double (0.0);
 
@@ -512,4 +607,63 @@ for x = 1: m
         
     end
 end
+end
+
+function im = normalize_image(img)
+
+
+mean = get_mean(img);
+sd = get_standard_deviation(img,mean);
+
+[m n] = size(img);
+sum = 0;
+
+
+for i = 1:m
+    for j = 1:n
+        if sd == 0
+            im(i,j) =  double(double(img(i,j)) - double(mean));
+        else
+            im(i,j) = double(double(img(i,j)) - double(mean))/sd;
+        end
+    end
+end
+
+
+end
+
+function mean = get_mean(img)
+
+[m n] = size(img);
+s = int32(0);
+
+for i = 1:m
+    for j = 1:n
+        
+        s = s + int32(img(i,j));
+        
+    end
+end
+
+mean = uint8(s/(m*n));
+
+end
+
+function sd = get_standard_deviation(img,mean)
+
+sum = int32(0);
+subtraction = 0;
+[m n] = size(img);
+for i = 1:m
+    for j = 1:n
+        
+        a = img(i,j);
+        b = double(a) - double(mean);
+        subtraction = b;
+        sum = sum + int32(subtraction)^2;
+        
+    end
+end
+sd = sum/int32(m*n);
+sd = sqrt(double(sd));
 end
