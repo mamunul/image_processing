5088144d8499f2f0a448fcc8be2cbf25c3241c83
mamunul <mamunul@192.168.1.135>
2016-01-26 10:33:48 +0600

    implementation of the cascade

--- violajones.m
+++ violajones.m
@@ -1,18 +1,268 @@
 
 function P = viola_jones()
 
+
+
+
+docNode = com.mathworks.xml.XMLUtils.createDocument('face-detection');
+
+entry_node = docNode.createElement('feature-list');
+
+
+
+
+
 PositiveTrainingPath = 'faces';
 NegativeTrainingPath = 'nonfaces';
 
+
 starttime = generate_datetime();
+trainig_face_limit = 20;
 
-trainig_face_limit = 100;
-
-% trainig_face_limit = 4500;
+% trainig_face_limit = 2000;
 
 [I1 m] = readImages(PositiveTrainingPath,'pgm',trainig_face_limit);
 
 [I2 l] = readImages(NegativeTrainingPath,'pgm',trainig_face_limit);
+feature_limit = 0;
+feature_list= haarfeature(24,feature_limit);
+f = 0.50; %false positive rate per cascade layer
+d = 0.90; %detection rate per cascade layer
+Ft = 0.000006; %overall false positive rate
+F(1) = 1;
+D(1) = 1;
+i = 1;
+j = 0;
+new_thresh = 0;
+while F(i) > Ft
+    i = i+1;
+    n = 10;
+    F(i) = F(i-1);
+    new_thresh = 0;
+    while F(i) > f*F(i-1)
+        rrr=F(i) > f*F(i-1)
+        n = n+10;
+        current_cascade = adaboost(I1,I2,feature_list,n);
+        [p q] = size(current_cascade);
+        for s = 1:p
+        cascade(1:s,:) = current_cascade(1:s,:);
+        [cf cd] = evaluate_cascade(cascade,I1,I2);
+        
+        F(i) = cf;
+        D(i) = cd;
+        new_thresh = decrease_threshold(d*D(i-1),cascade,I1);
+        
+        [p q] = size(cascade);
+    
+        
+        cascade(p,2) = new_thresh;
+        
+        [cf cd] = evaluate_cascade(cascade,I1,I2);
+        
+        F(i) = cf;
+        D(i) = cd;
+        
+        if F(i) <= f*F(i-1) && cd >= d*D(i-1)
+            break;
+        end
+        
+        end
+    end
+   
+    if F(i) > Ft
+        
+        I2 = evaluate_on_nonface(cascade,I2);
+        
+    end
+  cascade_node  = docNode.createElement('cascade');
+    for j = 1:s
+        feature_node =  create_feature_node(docNode,cascade_node,cascade(j,:));
+        cascade_node.appendChild(feature_node);
+    end
+     entry_node.appendChild(cascade_node);
+
+end
+endtime = generate_datetime();
+
+
+
+docNode.getDocumentElement.appendChild(entry_node);
+sin11 = create_single_node(docNode,entry_node,'code-link','https://github.com/mamunul/image_processing.git');
+docNode.getDocumentElement.appendChild(sin11);
+
+sin12 = create_single_node(docNode,entry_node,'trainig-face-quantity',trainig_face_limit);
+docNode.getDocumentElement.appendChild(sin12);
+
+sin14 = create_single_node(docNode,entry_node,'trainig-nonface-quantity',trainig_face_limit);
+docNode.getDocumentElement.appendChild(sin14);
+
+sin13 = create_single_node(docNode,entry_node,'feature-count',feature_limit);
+docNode.getDocumentElement.appendChild(sin13);
+
+sin1 = create_single_node(docNode,entry_node,'count',i-1);
+docNode.getDocumentElement.appendChild(sin1);
+%
+sin2 = create_single_node(docNode,entry_node,'start-time',starttime);
+docNode.getDocumentElement.appendChild(sin2);
+%
+sin3 = create_single_node(docNode,entry_node,'end-time',endtime);
+docNode.getDocumentElement.appendChild(sin3);
+
+xmlFileName = ['facedetection','.xml'];
+xmlwrite(xmlFileName,docNode);
+type(xmlFileName);
+
+% generateTrainingDb(current_cascade,starttime,endtime,trainig_face_limit,feature_count);
+
+end
+
+
+function N_I2 = evaluate_on_nonface(current_cascade,I2)
+[m p n] = size(I2);
+
+
+ j=0;
+    for i = 1:n
+        
+        cf = detect_face(current_cascade,I2(:,:,i),0);
+        
+        
+        if cf == 1
+            j = j +1;
+            II = I2(:,:,i);
+            
+            N_I2(:,:,j) = II;
+        end
+    end
+   
+
+end
+
+function  best_threshold =decrease_threshold(required_d,current_cascade,I1)
+
+
+[m p n] = size(I1);
+
+[p q] = size(current_cascade);
+
+classifier_threshold = current_cascade(p,2);
+
+min_thresh = 0;
+max_thresh = classifier_threshold;
+threshold = 0;
+cd = 0;
+
+bestcd = 0;
+best_threshold = 0;
+
+while (floor(max_thresh*1000) - floor(min_thresh*1000)) ~=0 
+    threshold = (max_thresh + min_thresh)/2;
+%     threshold = 0.8472;
+    cd = 0;
+    for i = 1:n
+        
+        cd = cd+ detect_face(current_cascade,I1(:,:,i),threshold);
+        
+    end
+    cd = cd/n;
+    
+    
+    if cd>bestcd
+        bestcd = cd;
+        best_threshold = threshold;
+    end
+    
+    if cd == required_d
+        break;
+    end
+    
+%     if floor(cd*100) < int8(required_d *100)
+%         max_thresh = threshold;
+%     elseif floor(cd*100) > int8(required_d *100)
+%         min_thresh = threshold;
+
+
+
+    if (cd) < (required_d)
+        max_thresh = threshold;
+    elseif (cd) > (required_d)
+        min_thresh = threshold;
+    end
+    
+end
+end
+
+function [cf cd] = evaluate_cascade(current_cascade,I1,I2)
+
+[m p n] = size(I1);
+cd = 0;
+cf = 0;
+for i = 1:n
+    
+    cd = cd + detect_face(current_cascade,I1(:,:,i),0);
+    
+end
+
+cd = cd/n;
+
+[m q n] = size(I2);
+
+for i = 1:n
+    
+    cf = cf + detect_face(current_cascade,I2(:,:,i),0);
+    
+end
+
+cf = cf/n;
+
+end
+
+function r = detect_face(current_cascade,I,threshold)
+[m n] = size(current_cascade);
+
+result = 0;
+for i = 1:m
+    
+    classifier = current_cascade(i,:);
+    f(1) = classifier(8);
+    f(2) = classifier(9);
+    f(3) = classifier(12);
+    f(4) = classifier(13);
+    f(5) = classifier(10);
+    f(6) = classifier(11);
+    
+    theta = classifier(4);
+    alpha = classifier(5);
+    beta = classifier(6);
+    polarity = classifier(7);
+    
+    classifier_threshold = current_cascade(m,2);
+    
+    f_v = calculate_feature_value(I,f,1);
+    
+    
+    
+    if polarity*f_v(1)  < polarity*theta  % confusion on polarity
+        result = result + alpha;
+        
+    end
+end
+
+if threshold ~= 0
+    classifier_threshold = threshold;
+end
+
+if result < classifier_threshold
+    r = 0;
+else
+    r = 1;
+end
+end
+
+function result = adaboost(I1,I2,feature_list,NumberOfClassifier)
+
+[dgd jk m] = size(I1);
+[ertt jk l] = size(I2);
 
 I = cat(3,I1,I2);
 
@@ -20,13 +270,9 @@ y1 = ones(1,m);
 y2 = zeros(1,l);
 y = [y1,y2];
 
-feature_count = 20000;
-feature_list= haarfeature(24,feature_count);
+
 
 [feature_count n] = size(feature_list);
-
-
-
 
 i = 0;
 
@@ -39,25 +285,23 @@ for f_no =1:feature_count
     feature_values(1:(m+l),1:2,f_no) = f_I;
 end
 
-
-NumberOfClassifier = 50;
-%     max = 3;
 result = zeros(NumberOfClassifier,13);
 w(1,1:m+l) = initialize_weight(m,l);
+
+classifier_threshold = 0;
 for t = 1:NumberOfClassifier
     
     w(t,1:m+l) = normalize_weight(w(t,:));
     
     [Tp Tm] = calculate_tpm1(w(t,:),y);
     
-   
-    
     err = 100;
     thresh = 0;
     polar = 0;
+    s_f_no = 0;
+    
+    
     for f_no =1:feature_count
-        
-       
         
         feature = feature_list(f_no,:);
         
@@ -65,46 +309,44 @@ for t = 1:NumberOfClassifier
         
         fv_weight = fvplusweight(values,w(t,:));
         
-
-        [err thresh polar] = bestfeature(feature,fv_weight,Tp,Tm,err,thresh,polar);
-
+        [err thresh polar] = bestfeature(feature,fv_weight,Tp,Tm,err);
+        
+        if thresh ~= -100
+            beta(t) = calc_beta(err);
+            alpha(t) = calc_alpha(beta(t));
+            error(t) = err;
+            theta(t)=thresh;
+            polarity(t)=polar;
+            best_feature = feature;
+            best_values = values;
+        end
+        
     end
-  
-
-    beta(t) = calc_beta(err);
-    alpha(t) = calc_alpha(beta(t));
-    error(t) = err;
-    theta(t)=thresh;
-    polarity(t)=polar;
-    w(t+1,:) = calc_weight(w(t,:),beta(t),values,thresh,polar);
+    
+    w(t+1,:) = calc_weight(w(t,:),beta(t),best_values,theta(t),polarity(t));
     
     display('classifier no------------');
     disp(t);
-
-    value_no = t;
-    value_x = double(feature(1));
-    value_y = double(feature(2));
-    value_fw = double(feature(5));
-    value_fh = double(feature(6));
-    value_w = double(feature(3));
-    value_h = double(feature(4));
     
-   
-    result(t,:)=[t,t,err,thresh,alpha(t),beta(t) ,polar,value_x ,value_y,value_fw ,value_fh,value_w ,value_h];
+    value_no = t;
+    value_x = double(best_feature(1));
+    value_y = double(best_feature(2));
+    value_fw = double(best_feature(5));
+    value_fh = double(best_feature(6));
+    value_w = double(best_feature(3));
+    value_h = double(best_feature(4));
+    
+    
+    classifier_threshold = classifier_threshold + alpha(t);
+    
+    result(t,:)=[t,classifier_threshold,err,theta(t),alpha(t),beta(t) ,polarity(t),value_x ,value_y,value_fw ,value_fh,value_w ,value_h];
     %     end
     
 end
-
-
-% C = sortrows(result,4);
-
-% C=result(result(:,4) >30, :);
-B = sortrows(result,3);
-R = B(1:10000,:);
-
-endtime = generate_datetime();
-generateTrainingDb(R,starttime,endtime,trainig_face_limit,feature_count);
-
+% for t = 1:NumberOfClassifier
+%     
+%     result(t,2) = classifier_threshold;
+% end
 end
 
 function fvw = fvplusweight(v,w)
@@ -127,15 +369,16 @@ beta = error/(1-error);
 
 end
 
-function [error thresh polarity] = bestfeature(feature,feature_value,tp,tm,error,thresh,polarity)
+function [error thresh polarity] = bestfeature(feature,feature_value,tp,tm,error)
 sp = 0;
 sm = 0;
 [m n]=size(feature_value);
 feature_value = sortrows(feature_value,1);
-
+thresh = -100;
+polarity = -100;
 for i = 1:m
     
-
+    
     
     if feature_value(i,2) == 1
         sp = sp + feature_value(i,3);
@@ -148,7 +391,7 @@ for i = 1:m
     
     e = min(v1,v2);
     
-       
+    
     if e<error
         error = e;
         thresh = feature_value(i,1);
@@ -171,13 +414,13 @@ tm = 0;
 [m n] = size(w);
 
 for i = 1:n
-if y(i) == 1
-    tp = tp+ w(i);
- 
-else
-    tm = tm + w(i);
-  
-end
+    if y(i) == 1
+        tp = tp+ w(i);
+        
+    else
+        tm = tm + w(i);
+        
+    end
 end
 
 end
@@ -189,13 +432,13 @@ tm = 0;
 [m n] = size(w);
 w_p = 0;w_n = 0;
 for i = 1:n
-if y(i) == 1
-    tp = tp+ w(i)*values(i);
-    w_p = w_p + w(i);
-else
-    tm = tm + w(i)*values(i);
-    w_n = w_n + w(i);
-end
+    if y(i) == 1
+        tp = tp+ w(i)*values(i);
+        w_p = w_p + w(i);
+    else
+        tm = tm + w(i)*values(i);
+        w_n = w_n + w(i);
+    end
 end
 
 tp = tp/w_p;
@@ -274,11 +517,11 @@ end
 function feature_node = create_feature_node(docNode,entry_node,f)
 
 rf_no = f(1);
-lowest_t = f(2);
+threshold = f(2);
 error = f(3);
 theta = f(4);
 alpha = f(5);
-epsilon = f(6);
+beta = f(6);
 polarity = f(7);
 value_x = f(8);
 value_y = f(9);
@@ -290,9 +533,10 @@ value_h = f(13);
 feature_node = docNode.createElement('feature');
 entry_node.appendChild(feature_node);
 
+sin0 = create_single_node(docNode,feature_node,'threshold',threshold);
 sin1 = create_single_node(docNode,feature_node,'error',error);
 sin2 = create_single_node(docNode,feature_node,'theta',theta);
-
+sin3 = create_single_node(docNode,feature_node,'alpha',alpha);
 sin4 = create_single_node(docNode,feature_node,'polarity',polarity);
 sin5 = create_single_node(docNode,feature_node,'x',value_x);
 sin6 = create_single_node(docNode,feature_node,'y',value_y);
@@ -301,9 +545,10 @@ sin8 = create_single_node(docNode,feature_node,'fh',value_fh);
 sin9 = create_single_node(docNode,feature_node,'w',value_w);
 sin10 = create_single_node(docNode,feature_node,'h',value_h);
 
+feature_node.appendChild(sin0);
 feature_node.appendChild(sin1);
 feature_node.appendChild(sin2);
-
+feature_node.appendChild(sin3);
 feature_node.appendChild(sin4);
 feature_node.appendChild(sin5);
 feature_node.appendChild(sin6);
@@ -335,7 +580,7 @@ for i = 1:size(TrainFiles,1)
         I(:,:,m) = intimg(24,image);
         
         if m == 10
-        skjfkj=0;
+            skjfkj=0;
         end
         
         if m == limit
@@ -350,14 +595,14 @@ function nw = calc_weight(w,beta,values,thresh,p)
 
 [a b] = size(w);
 for i = 1:b
-   
+    
     if (p * values(i,1)) < (p * thresh)
         c = 1;
     else
         c = 0;
     end
     if values(i,2) == c
-  
+        
         nw(i) = w(i) * beta;
         
     else
@@ -465,23 +710,23 @@ for f=1:featureQuantity
                     feature_list(t,5) = fw;
                     feature_list(t,6) = fh;
                     
-                    if t ==limit
+                    if t == limit
                         break;
                     end
                 end
-                if t ==limit
+                if t == limit
                     break;
                 end
             end
-            if t ==limit
+            if t == limit
                 break;
             end
         end
-        if t ==limit
+        if t == limit
             break;
         end
     end
-    if t ==limit
+    if t == limit
         break;
     end
     
@@ -554,9 +799,7 @@ for i=1:number_of_image
         Ar = AN1-AN2;
     end
     
-    if abs(Ar) > 3
-        jk=0;
-    end
+    
     f_value(i,1) = Ar;
     f_value(i,2) = label(i);
 end
@@ -624,7 +867,7 @@ for i = 1:m
         if sd == 0
             im(i,j) =  double(double(img(i,j)) - double(mean));
         else
-            im(i,j) = double(double(img(i,j)) - double(mean))/sd;
+            im(i,j) = double(double(img(i,j)) - double(mean))/(2*sd);
         end
     end
 end
